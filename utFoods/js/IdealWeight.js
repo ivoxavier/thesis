f2690f25a80a31fcbc18a7a4d7ea97f4072c7c6e
ivoxavier <ivofernandes12@gmail.com>
2022-04-29 19:20:47 +0100

    fix typo

--- utFoods/js/IdealWeight.js
+++ utFoods/js/IdealWeight.js
@@ -1,11 +1,11 @@
 /*
- * 2021  Ivo Xavier
+ * 2022  Ivo Fernandes <pg27165@alunos.uminho.pt>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * kaltracker is distributed in the hope that it will be useful,
+ * utFoods is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
